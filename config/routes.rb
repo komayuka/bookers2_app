bbd80c7e453bdb0f201ab4eb059c52ebb2f8eb58
komayuka <komayuka115@gmail.com>
2021-03-12 10:46:40 +0000

    [progress]1

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
 
-  resources :users, only: [:show]
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
