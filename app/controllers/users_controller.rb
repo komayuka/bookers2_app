5dd42707246f3de73ed117700ba00399a6bf51fb
komayuka <komayuka115@gmail.com>
2021-03-13 07:25:07 +0000

    [progress]1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,30 @@
 class UsersController < ApplicationController
 
-  def new
+  def index
+    @user = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def create
   end
 
-  def index
+  def edit
+    @user = User.find(params[:id])
   end
 
-  def show
+  def update
   end
 
   def destroy
   end
 
+private
+
+def user_params
+  params.require(:user).permit(:name, :introduction)
+end
 
 end
