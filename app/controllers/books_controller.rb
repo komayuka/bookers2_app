476a8aa48b446b1112b72dffb0886fec9f896f45
komayuka <komayuka115@gmail.com>
2021-03-13 05:30:15 +0000

    [progress]1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,31 @@
 class BooksController < ApplicationController
 
+  def index
+   @books = Book.all
+   @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+  end
+
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+     redireck_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
   end
 
   end
