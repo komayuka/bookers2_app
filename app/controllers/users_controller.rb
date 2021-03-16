3810a7c4d2e576c2ae33f925fcf901e75053bd44
komayuka <komayuka115@gmail.com>
2021-03-16 05:41:04 +0000

    [progress]1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @user = User.all
@@ -16,10 +17,10 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    if current_user == @user
-      flash[:succces] = 'You have updated user successfully'
-      render :index
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
     end
   end
 
@@ -29,7 +30,7 @@ class UsersController < ApplicationController
 private
 
 def user_params
-  params.require(:user).permit(:name, :introduction)
+  params.require(:user).permit(:name, :profile_image, :introduction)
 end
 
 end
