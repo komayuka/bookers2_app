6ebc5cf968aa7cc273c238da02da8aa402bb13c0
komayuka <komayuka115@gmail.com>
2021-04-04 04:38:10 +0000

    [progress]

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
-
+  has_many :book_comment, dependent: :destroy
 
 end
