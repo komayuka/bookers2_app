194008e10e29bb1ce1d771680984c1e2506b3754
komayuka <komayuka115@gmail.com>
2021-04-06 23:48:01 +0000

    [task5]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get 'searchs/search' => 'searchs#search'
   devise_for :users
   resources :users do
     resource :relationships, only: [:create, :destroy]
